cdb07e86220982cb957463feb81d2c759e9da33a
Aerethos <87134976+Aerethos@users.noreply.github.com>
2026-03-31 17:22:42 +0100

    Update layout.tsx

--- app/portal/layout.tsx
+++ app/portal/layout.tsx
@@ -1,11 +1,3 @@
-'use client';
-
-import { PortalProvider } from '@/lib/portal-context';
-
 export default function PortalLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <PortalProvider>
-      {children}
-    </PortalProvider>
-  );
+  return <>{children}</>;
 }
